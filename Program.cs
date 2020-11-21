2692c17ede3363eebd620ccc47f3b2d88fd9a06e
Nikolay-78 <kalyam@mail.ru>
2020-11-21 19:15:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Module_11_СSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Николай";
+            byte MyAge = 42;
+            bool Pet = true;
+            double MyShoeSize = 46;
+                        
+            Console.WriteLine("Меня зовут " + MyName);
+            Console.WriteLine("Мне " + MyAge + " года");
+            Console.WriteLine("У меня есть питомец и это " + Pet);
+            Console.WriteLine("Мой размер обуви - " + MyShoeSize);
+            Console.ReadKey();
+        }
+    }
+}
